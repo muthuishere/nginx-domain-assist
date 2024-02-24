4899feb6608575c8f2c897cd4529f7a7022ae0a7
Muthukumaran Navaneethakrishnan <muthuishere@gmail.com>
2024-02-24 22:39:02 +0530

    updated tests

--- src/delete_domain_handler.js
+++ src/delete_domain_handler.js
@@ -10,21 +10,9 @@ export async function deleteSiteInNginx({nginxFolder, site,domain}) {
     if(!!domain){
          site=domain;
     }
+    deleteDomainInNginx({nginxFolder,domain:site})
 
-    const nginxSitesAvailableFolder = nginxFolder + Path.sep + 'sites-available'
-    const nginxSitesEnabledFolder = nginxFolder + Path.sep + 'sites-enabled'
-    let siteAvailableFile = Path.join(nginxSitesAvailableFolder, site + '.conf');
-    let siteEnabledLink = Path.join(nginxSitesEnabledFolder, site + '.conf');
 
-    if(fs.existsSync(siteAvailableFile) === false){
-        throw new Error("Site "+site+" not found or its available in a different conf file")
-    }
-
-    await deleteFile(siteEnabledLink)
-    await deleteFile(siteAvailableFile)
-    await removeSSL(site)
-    await restartNginx()
-    console.log("Deleted site & SSL for : " + site );
 }
 
 
--- src/static_domain_handler.test.js
+++ src/static_domain_handler.test.js
@@ -1,26 +1,56 @@
-import {expect} from "chai";
-import {getHomeFolder} from "./shared/system_processor.js";
 import * as Path from "path";
-import {createStaticDomain} from "./static_domain_handler.js";
-import {readFile} from "./shared/files.js";
 import fs from "fs";
 
 
-describe('addition test',   function() {
+// describe('addition test',   function() {
+//
+//
+//
+//         it('should create example.com.conf', async () => {
+//
+//             const nginxFolder = Path.join(getHomeFolder() , "test/resources/nginx") ;
+//             const staticPath = Path.join(getHomeFolder() , "test/resources/www") ;
+//             await createStaticDomain({nginxFolder: nginxFolder, domain: "staticdomain.com", path:staticPath, useSSL: true})
+//
+//             let filename = Path.join(nginxFolder, "sites-available", "staticdomain.com.conf");
+//             expect(fs.existsSync(filename)).to.be.true;
+//             const contents = await readFile(filename)
+//             expect(contents).to.contain("server_name staticdomain.com")
+//             expect(contents).to.contain("root "+staticPath)
+//
+//         });
+// });
+//
+
+import { expect } from 'chai';
+import { createStaticDomain } from "./static_domain_handler.js";
+import { getHomeFolder } from "./shared/system_processor.js";
+import { readFile } from "./shared/files.js";
+
+describe('createStaticDomain', () => {
+    it('should create a new static domain with SSL', async () => {
+        const nginxFolder = Path.join(getHomeFolder(), "test/resources/nginx");
+        const staticPath = Path.join(getHomeFolder(), "test/resources/www");
+        await createStaticDomain({ nginxFolder: nginxFolder, domain: "newdomain.com", path: staticPath, useSSL: true });
+
+        let filename = Path.join(nginxFolder, "sites-available", "newdomain.com.conf");
+        expect(fs.existsSync(filename)).to.be.true;
+        const contents = await readFile(filename);
+        expect(contents).to.contain("server_name newdomain.com");
+        expect(contents).to.contain("root " + staticPath);
+    });
 
 
 
-        it('should create example.com.conf', async () => {
+    it('should create a new static domain without SSL', async () => {
+        const nginxFolder = Path.join(getHomeFolder(), "test/resources/nginx");
+        const staticPath = Path.join(getHomeFolder(), "test/resources/www");
+        await createStaticDomain({ nginxFolder: nginxFolder, domain: "newdomain.com", path: staticPath, useSSL: false });
 
-            const nginxFolder = Path.join(getHomeFolder() , "test/resources/nginx") ;
-            const staticPath = Path.join(getHomeFolder() , "test/resources/www") ;
-            await createStaticDomain({nginxFolder: nginxFolder, domain: "staticdomain.com", path:staticPath, useSSL: true})
-
-            let filename = Path.join(nginxFolder, "sites-available", "staticdomain.com.conf");
-            expect(fs.existsSync(filename)).to.be.true;
-            const contents = await readFile(filename)
-            expect(contents).to.contain("server_name staticdomain.com")
-            expect(contents).to.contain("root "+staticPath)
-
-        });
+        let filename = Path.join(nginxFolder, "sites-available", "newdomain.com.conf");
+        expect(fs.existsSync(filename)).to.be.true;
+        const contents = await readFile(filename);
+        expect(contents).to.contain("server_name newdomain.com");
+        expect(contents).to.contain("root " + staticPath);
+    });
 });
